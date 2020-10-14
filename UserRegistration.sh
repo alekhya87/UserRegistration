019f99923d3cf9d2d1bcd5eebe060b7566faf195
alekhya87 <davulurialekhya87909.com>
2020-10-14 12:38:40 +0530

    UseCase-3 is done

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -18,3 +18,12 @@ then
 else
    echo "invalied last name"
 fi
+#usecase-3
+read -p "enter email address : " email
+emailPattern="^[0-9A-Za-z]+([.-_+][0-9A-Za-z]+)*@[0-9A-Za-z]+.[a-zA-Z]{2,4}|.[A-Za-z]{2}$"
+if [[ $email =~ $emailPattern ]]
+then
+	echo "valid email"
+else
+	echo "invalid email"
+fi
