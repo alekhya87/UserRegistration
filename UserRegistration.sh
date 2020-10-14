22b70bcd6574cdcd9909e35e98a1440dc404ebab
alekhya87 <davulurialekhya87909.com>
2020-10-14 12:25:46 +0530

    UseCase-1 is done

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash/ -x
 echo " User Registration"
-
+#usecase-1
+read -p "enter valied first name : " firstName
+firstNamePattern="^[a-zA-Z]{3,}$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "valied first name "
+else
+	echo "invalied first name"
+fi
